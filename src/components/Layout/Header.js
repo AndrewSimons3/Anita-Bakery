1136f8a65295b63b568f0ed0295abd67fd2edb0e
AndrewSimons3 <simdrew07@gmail.com>
2021-05-10 19:28:02 -0500

    added headerCartButton component and imported to Header component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,17 @@
 import { Fragment } from 'react';
 
-import anitaImage from '../../assets/meals.jpg';
+// import anitaImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = props => {
   return <Fragment>
     <header className={classes.header}>
       <h1>Anita's Bakery</h1>
-      <button>Cart</button>
+      <HeaderCartButton />
     </header>
     <div className={classes['main-image']}>
-      <img src={anitaImage} alt="table of Anita's baking goods!" />
+      {/* <img src={anitaImage} alt="table of Anita's baking goods!" /> */}
     </div>
   </Fragment>
 };
