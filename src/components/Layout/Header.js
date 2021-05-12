880869664df934383a98d84e969372251f259b1a
AndrewSimons3 <simdrew07@gmail.com>
2021-05-12 07:16:18 -0500

    added logic to hide show modal and open when clicking cart button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props => {
   return <Fragment>
     <header className={classes.header}>
       <h1 className={classes.title}>Anita's Bakery</h1>
-      <HeaderCartButton />
+      <HeaderCartButton onClick={props.onShowCart}/>
     </header>
     <div className={classes['main-image']}>
       <img src={anitaImage} alt="table of Anita's baking goods!" />
