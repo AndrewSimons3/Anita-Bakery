34ee6267fce8fb9ca46e74da8c0f291a3243fb43
AndrewSimons3 <simdrew07@gmail.com>
2021-05-11 22:09:30 -0500

    modal component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import Header from './components/Layout/Header';
 import Goods from './components/Goods/Goods';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
    <Fragment>
+     <Cart />
      <Header />
      <main>
       <Goods />
